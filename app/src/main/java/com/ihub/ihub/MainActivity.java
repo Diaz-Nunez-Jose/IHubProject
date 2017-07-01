7944ac7aa22f4bf467b5adc8957c099a4ab92897
jdn-jdn <dia15012@byui.edu>
2017-07-01 10:38:36 -0600

    Added working map activity

--- app/src/main/java/com/ihub/ihub/MainActivity.java
+++ app/src/main/java/com/ihub/ihub/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ihub.ihub;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void navMap(View view) {
+        Intent intent = new Intent(this, map.class);
+        startActivity(intent);
+    }
+
+    public void navTodo(View view) {
+        Intent intent = new Intent(this, todo.class);
+        startActivity(intent);
     }
 }
